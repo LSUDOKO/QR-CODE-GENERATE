78c962ae37779424f9536bb0c7d081023f6ab0fb
ARPIT SINGH <adoranto737@gmail.com>
2025-01-19 12:19:56 +0530

    Update qrcode7.py

--- qrcode7.py
+++ qrcode7.py
@@ -2,9 +2,9 @@
 
 import segno
 from urllib.request import urlopen
-slts_qrcode = segno.make_qr("https://github.com/LSUDOKO")
+qrcode = segno.make_qr("https://github.com/LSUDOKO")
 nirvana_url = urlopen("https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExaTlsbmE0a3FkOTdnYzEzcDE5OGliM2gxYXQwZHV0Ym04OG5jajBwbiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/NytMLKyiaIh6VH9SPm/giphy.webp")
-slts_qrcode.to_artistic(
+qrcode.to_artistic(
     background=nirvana_url,
     target="animated_qrcode.gif",
     scale=5,
